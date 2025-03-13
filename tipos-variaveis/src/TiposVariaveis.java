e3fa9d2bd1660d82daf2fbe56a787a07fa54aebc
PericlesNunes2022 <pericles.nunes2022@gmail.com>
2025-03-13 12:15:41 -0300

    operações

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        short numeroCurto = 1;
+        int numeronormal = numeroCurto;
+        short numeroCurto2 = tnumeronormal;
+
+        //quando ovalor nuca podera mudar?
+        //utilizando ovalor da variavel
+        // ex  
+        //final double vlor de PI = 10.75;
+
+        final double VALOR DE PI = 3.14;
+
+        //tipos primitivos
+        //estudar classe String meuNome = "Pericles"
     }
 }
